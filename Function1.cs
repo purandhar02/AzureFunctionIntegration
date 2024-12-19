fb0f6f2789912b723bdff64e4fc4b14470473f40
Purandhar Kola <purandhar.kola@cognine.com>
2024-12-19 14:55:43 +0530

    Add project files.

---
+++ Function1.cs
@@ -0,0 +1,34 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+namespace AzureFunctionIntegration
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("Function1")]
+        public static async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestData req,
+            FunctionContext context)
+        {
+            var logger = context.GetLogger("MyFunction");
+            logger.LogInformation("HTTP trigger function processed a request.");
+
+            // Extract query parameters from the URL
+            var queryParams = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
+            var name = queryParams["name"] ?? "Azure";
+
+            // Create a response
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            await response.WriteStringAsync($"Hello, {name}");
+            return response;
+        }
+    }
+}
